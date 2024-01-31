f62ed5c93200f481e8035545f2afa957ec254480
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-01-31 18:41:50 +0100

    Portfolio visibile solo se registrato + aggiunta CRUD

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,10 +8,17 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+    <a href="{{ route('admin.portfolios.create') }}">Aggiungi</a>
     @for ($i=0; $i<sizeof($portfolios); $i++)
         <div>{{ $portfolios[$i]["title"] }}</div>
         <div>{{ $portfolios[$i]["description"] }}</div>
         <img src="{{ $portfolios[$i]["thumb"] }}" alt="">
+        <a href="{{ route('admin.portfolios.edit', $portfolios[$i]['id'] ) }}">Modifica</a>
+        <form action="{{ route('admin.portfolios.destroy', $portfolios[$i]['id'] ) }}" method="POST">
+            @csrf
+            @method("DELETE")
+            <input type="submit" value="Elimina">
+        </form>
     @endfor
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PortfolioController;
+    use Illuminate\Support\Facades\Route;
+	use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
+    use App\Http\Controllers\Admin\PortfolioController;
+	
+	/* ... */
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+	Route::get('/', function () {
+		return view('welcome');
+	});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+	Route::middleware(['auth'])
+		->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+		->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+		->group(function () {
+		
+			//Siamo nel gruppo quindi:
+			// - il percorso "/" diventa "admin/"
+			// - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+			Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+            Route::resource("portfolios", PortfolioController::class);
+	});
 
-Route::resource("portfolios", PortfolioController::class);
-
-require __DIR__.'/auth.php';
+	require __DIR__.'/auth.php';
